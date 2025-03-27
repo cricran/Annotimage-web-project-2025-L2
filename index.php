1c57d99478987822b1f6adf0822feba50139caf6
cricran <cricrancricran@gmail.com>
2025-03-28 00:38:52 +0100

    Enhance .htaccess and index.php for better URL handling and page structure; rename readme.md to readme.qmd for quarto better support

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $uri = trim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), "/");
 
 if ($uri === "") {
-    echo "<html><body><h1>Page Home</h1></body></html>";
+    echo "<html><head><link rel='icon' type='image/png' href='logo.png'><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Annotimage - Home</title></head><body><h1>Page Home</h1></body></html>";
 } elseif ($uri === "test" && isset($_GET["id"])) {
     echo "<html><body><h1>Page test</h1><p>{$_GET['id']}</p></body></html>";
 } else {
