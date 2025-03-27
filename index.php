4c1d66d55231f4895a29af207af475a21abcecad
cricran <cricrancricran@gmail.com>
2025-03-28 00:01:26 +0100

    premier test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $uri = trim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), "/");
 
 if ($uri === "") {
-    echo "<html><body><h1>Home</h1></body></html>";
+    echo "<html><body><h1>Page</h1></body></html>";
 } elseif ($uri === "test" && isset($_GET["id"])) {
     echo "<html><body><h1>Page test</h1><p>{$_GET['id']}</p></body></html>";
 } else {
@@ -10,3 +10,5 @@ if ($uri === "") {
     echo "<html><body><h1>Page introuvable</h1></body></html>";
 }
 ?>
+
+je teste des chose !
