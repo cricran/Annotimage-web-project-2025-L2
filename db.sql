c83609d5f99c01f9d7b36bc4a97403ba3996d5d3
cricran <cricrancricran@gmail.com>
2025-04-03 19:52:32 +0200

    Add initial database schema for user, image, annotation, and tag tables

---
+++ db.sql
@@ -0,0 +1,41 @@
+create table annotimage.user (
+	id int auto_increment primary key,
+	login varchar(64) not null,
+    email varchar(255) not null,
+    password varchar(64) not null
+)engine InnoDB;
+
+create table annotimage.image (
+	id int auto_increment primary key,
+    path varchar(255) not null,
+	description varchar(1024) not null,
+    public boolean default false,
+    date datetime not null,
+    userId int not null,
+    foreign key (userId) references user(id)
+)engine InnoDB;
+
+create table annotimage.annotation (
+	id int auto_increment primary key,
+    imageId int not null,
+    description varchar(1024) not null,
+    foreign key (imageId) references image(id)
+)engine InnoDB;
+
+create table annotimage.tag (
+	id int auto_increment primary key,
+	name varchar(64) not null
+)engine InnoDB;
+
+create table annotimage.taged (
+	imageId int,
+    tagId int,
+    foreign key (imageId) references image(id),
+    foreign key (tagId) references tag(id)
+)engine InnoDB;
+    
+
+
+
+
+
